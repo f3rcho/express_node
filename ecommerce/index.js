66a202e9ee2af65fb66ada726bb4cc9255d4bf9c
fernando <fernan942@gmail.com>
2020-08-07 11:49:59 -0400

    Using Pug as a template engine 7

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const productsRouter = require('./routes/products')
 
-app.get('*', (req, res, next) => {
-    res.json({ hello: 'World'});
-});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/products', productsRouter);
 
 const server = app.listen(8000, () => {
     console.log(`App listening on http://localhost:${server.address().port}`);
