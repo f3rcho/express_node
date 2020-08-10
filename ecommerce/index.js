a0e23083c6c9ac1de8d53f614ab32b99b67ea95e
fernando <fernan942@gmail.com>
2020-08-10 06:57:18 -0400

    Handling static files 10

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const productsRouter = require('./routes/products')
+const productsRouter = require('./routes/products');
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
